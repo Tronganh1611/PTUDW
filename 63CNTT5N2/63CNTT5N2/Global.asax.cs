259f58f172e8458da7d5a1fc9923cae97edaf253
Tronganh1611 <tronganh16112003@gmail.com>
2023-11-25 19:17:20 +0700

    menu vs mainmenu

--- 63CNTT5N2/63CNTT5N2/Global.asax.cs
+++ 63CNTT5N2/63CNTT5N2/Global.asax.cs
@@ -17,5 +17,9 @@ namespace _63CNTT5N2
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        public void Session_Start()
+        {
+            Session["UserID"] = "1";
+        }
     }
 }
